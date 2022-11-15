6d0a84d8727f8b7028f8f70e0d2f0301d8d3c0d9
LauCarrillo <laucarrillo21@gmail.com>
2022-11-15 16:30:01 -0600

    update API Integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -3,27 +3,29 @@ import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
-    let [keyword, setKeyword] = useState("");
+  let [keyword, setKeyword] = useState("");
 
-    function handleResponse(response){
-      console.log(response.data[0]);
-    }
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
 
-    function search(event) {
-        event.preventDefault();
+  function search(event) {
+    event.preventDefault();
 
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
-    }
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
 
-    function handleKeywordChange(event) {
-        setKeyword(event.target.value);
-    }
-  return <div className="Dictionary">
-    <form onSubmit={search}>
-        <input type="search" autoFocus={true} onChange={handleKeywordChange} />
-    </form>
-  </div>
+  return (
+    <div className="Dictionary">
+      <form onSubmit={search}>
+        <input type="search" onChange={handleKeywordChange} />
+      </form>
+    </div>
+  );
 }
