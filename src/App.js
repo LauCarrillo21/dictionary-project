ed835f7b2d30d6026a40edbfc218dbd2ac6b5205
LauCarrillo <laucarrillo21@gmail.com>
2022-11-14 19:05:31 -0600

    update dictionary search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Hello world
+
+      <header>
+        This is the header
       </header>
+
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className='App-footer'> 
+        <small>
+        Coded by <a href="https://www.linkedin.com/in/laura-carrillo21/"> Lau Carrillo </a>
+        </small>
+      </footer>
+
     </div>
   );
 }
