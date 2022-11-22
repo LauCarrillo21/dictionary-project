89fbbfece3c20dee90d1f3d060b7afd57e5b185d
LauCarrillo <laucarrillo21@gmail.com>
2022-11-21 19:53:50 -0600

    synonyms

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import './App.css';
-import Dictionary from './Dictionary';
+import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-
-      <header>
-        This is the header
-      </header>
-
-      <main>
-        <Dictionary />
-      </main>
-
-      <footer className='App-footer'> 
-        <small>
-        Coded by <a href="https://www.linkedin.com/in/laura-carrillo21/"> Lau Carrillo </a>
-        </small>
-      </footer>
-
+      <div className="container">
+        <header className="App-header">
+          <h1> Header</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Lau Carrillo</small>
+        </footer>
+      </div>
     </div>
   );
 }
