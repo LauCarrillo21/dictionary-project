24dfee77d82b7bd99a5a7aa7c87b0b8ad1439ff9
LauCarrillo <laucarrillo21@gmail.com>
2022-11-29 10:40:17 -0600

    update react app

--- src/Photos.js
+++ src/Photos.js
@@ -1,26 +1,24 @@
 import React from "react";
 import "./Photos.css";
 
-export default function Photos (props) {
-    if(props.photos) {
-        console.log(props.photos);
-        return <section className="Photos">
-            <div clasName="row">
-            {props.photos.map(function(photo, index){
-                return(
-                    <div clasName="col-3"  key={index}>
-                    <a href={photo.src.original} target="_blank"
-                    rel="noreferrer">
-                    <img src={photo.src.tiny}
-                    className="img-fluid"
-                    alt="dictionary-search"/>
-                    </a>
-                    </div>
-                )
-            })}
-            </div>
-            </section>;
-    } else {
-        return null;
-    }
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <section className="Photos">
+        <div className="row">
+          {props.photos.map(function (photo, index) {
+            return (
+              <div className="col-4" key={index}>
+                <a href={photo.src.original} target="_blank" rel="noreferrer">
+                  <img src={photo.src.landscape} className="img-fluid" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      </section>
+    );
+  } else {
+    return null;
+  }
 }
--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,19 +1,14 @@
 import React from "react";
+import "./Synonyms.css";
+
 export default function Synonyms(props) {
-  if (props.synonyms && props.synonyms.length > 0) {
+  if (props.synonyms) {
     return (
-      <div className="synonyms">
-        <h4>Synonyms:</h4>
-        <ul>
-          {props.synonyms.map(function (synonym, index) {
-            return (
-              <li key={index} className="synonym">
-                {synonym}
-              </li>
-            );
-          })}
-        </ul>
-      </div>
+      <ul className="Synonyms">
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>{synonym}</li>;
+        })}
+      </ul>
     );
   } else {
     return null;
